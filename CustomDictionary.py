7d63306647d05e4831c0902f4006df74b08bac4b
poorn <naidu.poorna10@gmail.com>
2020-12-20 13:29:40 -0600

    custom dictionary superclass added

--- CustomDictionary.py
+++ CustomDictionary.py
@@ -1,1 +1,11 @@
-print("hi!")
+referValues = {None : 0, int: 0, float: 0.0, str: 'NaN', list: list(),dict : dict(), set: set()}
+
+class CustomDictionary(dict):
+
+    def __init__(self,setValueType = None):
+        self.setValueType = setValueType
+
+    def __getitem__(self, key):
+        self.setdefault(key,referValues[self.setValueType])
+        return dict.__getitem__(self, key)
+
